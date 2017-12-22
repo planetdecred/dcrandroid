f097f368bbf2b0fc68afaec77f966e6b4d4d3eb8
Collins Olanrewaju <ocollins444@gmail.com>
2017-12-22 15:19:07 +0100

    Solved bug with config file not found

--- app/src/main/java/com/decrediton/MainActivity.java
+++ app/src/main/java/com/decrediton/MainActivity.java
@@ -35,6 +35,7 @@ import javax.security.auth.x500.X500Principal;
 
 import dcrwallet.Dcrwallet;
 import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.okhttp.NegotiationType;
 import io.grpc.okhttp.OkHttpChannelBuilder;
 
@@ -62,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     this.setPriority(MAX_PRIORITY);
                     System.out.println("Wallet Home Dir: "+Dcrwallet.getHomeDir());
-                    File file = new File(Dcrwallet.getHomeDir()+"/dcrwallet.conf");
+                    File path = new File(Dcrwallet.getHomeDir()+"/");
+                    path.mkdirs();
+                    File file = new File(path,"dcrwallet.conf");
                     if(!file.exists()) {
                         FileOutputStream fout = new FileOutputStream(file);
                         InputStream in = getAssets().open("sample-dcrwallet.conf");
@@ -75,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
                         fout.flush();
                         fout.close();
                     }
-                    //file.delete();
                     Dcrwallet.main();
                 }catch (Exception e){
                     e.printStackTrace();
@@ -86,62 +88,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    SSLSocketFactory factory = newSslSocketFactoryForCa(loadCert("rpc.pem"));
-                    ManagedChannel channel = OkHttpChannelBuilder.forAddress("127.0.0.1", 9111)
-                            .negotiationType(NegotiationType.TLS)
-                            .sslSocketFactory(factory)
+                    ManagedChannel channel = ManagedChannelBuilder.forAddress("127.0.0.1", 9111)
                             .usePlaintext(true)
                             .build();
-                    CreateWalletRequest walletRequest = CreateWalletRequest.newBuilder()
+                    VersionRequest versionRequest = VersionRequest.newBuilder()
                             .build();
-                    WalletLoaderServiceGrpc.WalletLoaderServiceBlockingStub walletLoaderServiceBlockingStub = WalletLoaderServiceGrpc.newBlockingStub(channel);
-                    CreateWalletResponse walletResponse = walletLoaderServiceBlockingStub.createWallet(walletRequest);
-                    System.out.println("Wallet Response is here");
-//                    VersionRequest versionRequest = VersionRequest.newBuilder()
-//                            .build();
-//                    VersionServiceGrpc.VersionServiceBlockingStub stub = VersionServiceGrpc.newBlockingStub(channel);
-//                    VersionResponse response = stub.version(versionRequest);
-//                    System.out.println("Version response: " + response.getVersionString() + " " + response.getMajor());
+                    VersionServiceGrpc.VersionServiceBlockingStub stub = VersionServiceGrpc.newBlockingStub(channel);
+                    VersionResponse response = stub.version(versionRequest);
+                    System.out.println("Version response: " + response.getVersionString() + " " + response.getMajor());
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
     }
-    public FileInputStream loadCert(String name) throws IOException {
-        InputStream in = getAssets().open("rpc.pem");
-        File tmpFile = File.createTempFile(name, "");
-        tmpFile.deleteOnExit();
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(tmpFile));
-        try {
-            int b;
-            while ((b = in.read()) != -1) {
-                writer.write(b);
-            }
-        } finally {
-            writer.close();
-        }
-        return new FileInputStream(tmpFile);
-    }
-
-    public SSLSocketFactory newSslSocketFactoryForCa(InputStream certChain) throws Exception {
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        ks.load(null, null);
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        X509Certificate cert = (X509Certificate) cf.generateCertificate(
-                new BufferedInputStream(certChain));
-        X500Principal principal = cert.getSubjectX500Principal();
-        ks.setCertificateEntry(principal.getName("RFC2253"), cert);
-
-        // Set up trust manager factory to use our key store.
-        TrustManagerFactory trustManagerFactory =
-                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(ks);
-        SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, trustManagerFactory.getTrustManagers(), null);
-        return context.getSocketFactory();
-    }
-
-
 }
