b4f3bd21dd6eb68c98b8e481b2c0951acf713c43
Collins Olanrewaju <ocollins444@gmail.com>
2017-12-23 23:54:51 +0100

    Code cleanup for  dcrwallet-connector

--- app/src/main/java/com/decrediton/MainActivity.java
+++ app/src/main/java/com/decrediton/MainActivity.java
@@ -1,43 +1,13 @@
 package com.decrediton;
 
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import com.dcrwallet.grpc.CreateWalletRequest;
-import com.dcrwallet.grpc.CreateWalletResponse;
-import com.dcrwallet.grpc.VersionRequest;
-import com.dcrwallet.grpc.VersionResponse;
-import com.dcrwallet.grpc.VersionServiceGrpc;
-import com.dcrwallet.grpc.WalletLoaderServiceGrpc;
-import com.dcrwallet.grpc.WalletServiceGrpc;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
-import javax.security.auth.x500.X500Principal;
-
 import dcrwallet.Dcrwallet;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.okhttp.NegotiationType;
-import io.grpc.okhttp.OkHttpChannelBuilder;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,18 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        File sdcard = new File("/sdcard");
-        File homeDir = new File(Dcrwallet.getHomeDir());
-        if(sdcard.exists()){
-            System.out.println("SDCard Exist");
-        }else{
-            System.out.println("SDCard does not exist");
-        }
-        if(homeDir.exists()){
-            System.out.println("Home Dir Exists");
-        }else{
-            System.out.println("Home Dir Does not exist");
-        }
         new Thread(){
             public void run(){
                 try {
@@ -87,19 +45,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_connect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    ManagedChannel channel = ManagedChannelBuilder.forAddress("127.0.0.1", 9111)
-                            .usePlaintext(true)
-                            .build();
-                    VersionRequest versionRequest = VersionRequest.newBuilder()
-                            .build();
-                    VersionServiceGrpc.VersionServiceBlockingStub stub = VersionServiceGrpc.newBlockingStub(channel);
-                    VersionResponse response = stub.version(versionRequest);
-                    System.out.println("Version response: " + response.getVersionString() + " " + response.getMajor());
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
             }
         });
     }
+
 }
