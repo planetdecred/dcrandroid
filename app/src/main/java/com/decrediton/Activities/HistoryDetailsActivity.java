527d48e6a64124ab1d8f7e6bbb5db730ea9ac0f0
macsleven <abubakars29@yahoo.com>
2018-01-02 17:34:32 +0100

    Transaction history list view module with design and Overview module

---
+++ app/src/main/java/com/decrediton/Activities/HistoryDetailsActivity.java
@@ -0,0 +1,10 @@
+package com.decrediton.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Macsleven on 01/01/2018.
+ */
+
+public class HistoryDetailsActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/decrediton/fragments/HistoryFragment.java
+++ app/src/main/java/com/decrediton/fragments/HistoryFragment.java
@@ -1,25 +1,66 @@
 package com.decrediton.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.decrediton.Activities.HistoryDetailsActivity;
+import com.decrediton.Adapter.HistoryAdapter;
 import com.decrediton.R;
+import com.decrediton.Util.RecyclerTouchListener;
+import com.decrediton.data.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Macsleven on 28/11/2017.
  */
 
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends Fragment{
+        private List<Transaction> historyList = new ArrayList<>();
+        HistoryAdapter historyAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.content_history, container, false);
+        View rootView = inflater.inflate(R.layout.content_history, container, false);
+        LayoutInflater layoutInflater = LayoutInflater.from(rootView.getContext());
+        RecyclerView recyclerView = rootView.getRootView().findViewById(R.id.history_recycler_view);
+        historyAdapter = new HistoryAdapter(historyList, layoutInflater);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(rootView.getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration( getContext(), LinearLayoutManager.VERTICAL));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Transaction history = historyList.get(position);
+                Intent i = new Intent(getContext(), HistoryDetailsActivity.class);
+                i.putExtra("Amount",history.getAccountName());
+                i.putExtra("Address",history.getAddress());
+                i.putExtra("TxDate",history.getTxDate());
+                i.putExtra("TxType",history.getTxType());
+                i.putExtra("AccountName",history.getAccountName());
+                i.putExtra("TxStatus",history.getTxStatus());
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+        recyclerView.setAdapter(historyAdapter);
+        registerForContextMenu(recyclerView);
+        prepareHistoryData();
+        return rootView;
     }
 
 
@@ -28,5 +69,21 @@ public class HistoryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("History");
+    }
+    private void prepareHistoryData(){
+        Transaction history= new Transaction("-120.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 20:19:45","pending","default","send");
+        historyList.add(history);
+        history= new Transaction("-120.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 11:17:25","pending","default","send");
+        historyList.add(history);
+        history= new Transaction("-100.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 19:19:45","pending","default","send");
+        historyList.add(history);
+        history= new Transaction("+220.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 22:12:32","confirmed","default","receive");
+        historyList.add(history);
+        history= new Transaction("+10.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 13:19:55","confirmed","default","send");
+        historyList.add(history);
+        history= new Transaction("+1200.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 20:19:51","confirmed","default","send");
+        historyList.add(history);
+        history= new Transaction("+200.0000000 DCR","Txsjdhfueyxhdgrthdjfhsverutif","jan 1 2018, 14:32:39","confirmed","default","receive");
+        historyList.add(history);
     }
 }
