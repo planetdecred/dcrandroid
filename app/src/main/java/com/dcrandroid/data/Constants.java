607c98a73efc1c92651fc35f612d37c13583124c
Afolabi michael <afomic1@gmail.com>
2018-03-04 22:39:48 +0100

    fixed confirmation on detail view issue (#31)
    Basic code cleanup

---
+++ app/src/main/java/com/dcrandroid/data/Constants.java
@@ -0,0 +1,19 @@
+package com.dcrandroid.data;
+
+/**
+ * Created by afomic on 2/28/18.
+ */
+
+public class Constants {
+    public static final String EXTRA_TRANSACTION_HASH="Hash";
+    public static final String EXTRA_TRANSACTION_CONFIRMATION="TXConfirmation";
+    public static final String EXTRA_TRANSACTION_FEE="Fee";
+    public static final String EXTRA_TRANSACTION_DATE="TxDate";
+    public static final String EXTRA_TRANSACTION_STATUS="TxStatus";
+    public static final String EXTRA_TRANSACTION_TYPE="TxType";
+    public static final String EXTRA_AMOUNT="Amount";
+    public static final String EXTRA_INPUT_USED="UsedInput";
+    public static final String EXTRA_NEW_WALLET_OUTPUT="newWalletOutPut";
+
+
+}
