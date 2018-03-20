c381438b439fadce1b8c884776a5413687f6206a
macsleven <abubakars29@yahoo.com>
2018-03-20 16:48:23 +0100

    centered toast message for transaction details view

--- app/src/main/java/com/dcrandroid/activities/TransactionDetailsActivity.java
+++ app/src/main/java/com/dcrandroid/activities/TransactionDetailsActivity.java
@@ -150,7 +150,7 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         }
         Toast toast = Toast.makeText(getApplicationContext(),
                 R.string.tx_hash_copy, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.BOTTOM | Gravity.RIGHT, 50, 50);
+        toast.setGravity(Gravity.CENTER | Gravity.START, 50, 50);
         toast.show();
     }
 }
