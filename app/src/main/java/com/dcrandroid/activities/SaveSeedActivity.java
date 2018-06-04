7ae827306e1b6d1f93b6a660357d5c1e7e16fb20
macsleven <abubakars29@yahoo.com>
2018-06-04 12:07:14 +0100

    seed display in scroll view for different screen size

--- app/src/main/java/com/dcrandroid/activities/SaveSeedActivity.java
+++ app/src/main/java/com/dcrandroid/activities/SaveSeedActivity.java
@@ -3,6 +3,7 @@ package com.dcrandroid.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import com.dcrandroid.util.DcrConstants;
 import com.dcrandroid.R;
+import com.dcrandroid.view.SeedLayout;
 
 
 /**
@@ -17,34 +19,47 @@ import com.dcrandroid.R;
  */
 
 public class SaveSeedActivity extends AppCompatActivity {
-    private TextView saveSeedTextView;
+    private SeedLayout saveSeedTextView;
     private String seed = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_seed_page);
         Button saveSeedContBtn = (Button)findViewById(R.id.save_seed_btn_continue);
-        saveSeedTextView = (TextView) findViewById(R.id.save_seed_text_view);
+        saveSeedTextView = (SeedLayout) findViewById(R.id.seedLayout);
+        LayoutInflater layoutInflater =getLayoutInflater();
+       String seed = "";
         DcrConstants constants = DcrConstants.getInstance();
         try {
             seed = constants.wallet.generateSeed();
-            saveSeedTextView.setText(seed);
+            String tempSeed[] = seed.split("");
+            for(int i = 0; i <= tempSeed.length-1; i++){
+                seed =tempSeed[i];
+                View seedView = layoutInflater.inflate(R.layout.seed_layout,null,false);
+                TextView seedTexView = seedView.findViewById(R.id.seedTextView);
+                seedTexView.setText(seed);
+                saveSeedTextView.addView(seedView);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
         saveSeedContBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!seed.equals("")) {
-                    Intent i = new Intent(SaveSeedActivity.this, ConfirmSeedActivity.class)
-                            .putExtra("seed", seed)
-                            .putExtra("restore", false);
-                    startActivity(i);
-                }else{
-                    Toast.makeText(SaveSeedActivity.this, R.string.error_seed_not_generated, Toast.LENGTH_SHORT).show();
-                }
+                btnCopyPhrase();
             }
         });
+    }
+
+    private void btnCopyPhrase(){
+        if(!seed.equals("")) {
+            Intent i = new Intent(SaveSeedActivity.this, ConfirmSeedActivity.class)
+                    .putExtra("seed", seed)
+                    .putExtra("restore", false);
+            startActivity(i);
+        }else{
+            Toast.makeText(SaveSeedActivity.this, R.string.error_seed_not_generated, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
