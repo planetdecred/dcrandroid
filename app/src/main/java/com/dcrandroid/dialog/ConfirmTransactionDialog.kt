bb1ed0476154b6871e918d57f97d2dbfea8779a4
SergeyMikhlevskiy <mobiwolf.dev@gmail.com>
2018-10-25 12:15:55 +0300

    change(ConfirmTransactionDialog)combined two dialogs into one
    Fixed issue - #163

--- app/src/main/java/com/dcrandroid/dialog/ConfirmTransactionDialog.kt
+++ app/src/main/java/com/dcrandroid/dialog/ConfirmTransactionDialog.kt
@@ -5,23 +5,31 @@ import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.Window
 import android.widget.TextView
 import com.dcrandroid.R
+import com.dcrandroid.data.Constants
+import com.dcrandroid.util.PreferenceUtil
 import com.dcrandroid.util.Utils
+import kotlinx.android.synthetic.main.confirm_tx_dialog.*
 import mobilewallet.Mobilewallet
 import java.text.DecimalFormat
 
-class ConfirmTransactionDialog(context: Context?) : Dialog(context), View.OnClickListener{
+class ConfirmTransactionDialog(context: Context?) : Dialog(context), View.OnClickListener {
 
-    private var btnPositiveClick : DialogInterface.OnClickListener? = null
+    private var btnPositiveClick: DialogInterface.OnClickListener? = null
 
-    private var amount : Long? = null
+    private var amount: Long? = null
 
-    private var address : CharSequence? = null
+    private var address: CharSequence? = null
 
-    private var fee : Long? = null
+    private var fee: Long? = null
+
+    private val util = PreferenceUtil(getContext())
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +45,7 @@ class ConfirmTransactionDialog(context: Context?) : Dialog(context), View.OnClic
 
         val tvTitle = findViewById<TextView>(R.id.title)
 
-        tvTitle.text =  context.getString(R.string.sending) + " ${format.format(Mobilewallet.amountCoin(amount!!))} DCR"
+        tvTitle.text = context.getString(R.string.sending) + " ${format.format(Mobilewallet.amountCoin(amount!!))} DCR"
 
         val tvAddress = findViewById<TextView>(R.id.address)
         tvAddress.text = address
@@ -50,39 +58,59 @@ class ConfirmTransactionDialog(context: Context?) : Dialog(context), View.OnClic
         val tvTotal = findViewById<TextView>(R.id.total)
 
         tvTotal.text = "Total ${Mobilewallet.amountCoin(amount!! + estFee)} DCR"
+
+        passphrase_input.addTextChangedListener(passphraseTextWatcher)
+
     }
 
-    fun setPositiveButton(text: String, listener: DialogInterface.OnClickListener?) : ConfirmTransactionDialog{
+    fun setPositiveButton(listener: DialogInterface.OnClickListener?): ConfirmTransactionDialog {
         btnPositiveClick = listener
         return this
     }
 
-    fun setAmount(amount : Long) : ConfirmTransactionDialog{
+    fun setAmount(amount: Long): ConfirmTransactionDialog {
         this.amount = amount
         return this
     }
 
-    fun setAddress(address: String) : ConfirmTransactionDialog{
+    fun setAddress(address: String): ConfirmTransactionDialog {
         this.address = address
         return this
     }
 
-    fun setFee(fee: Long) : ConfirmTransactionDialog{
+    fun setFee(fee: Long): ConfirmTransactionDialog {
         this.fee = fee
         return this
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id){
+        when (v!!.id) {
             R.id.btn_negative -> {
                 cancel()
             }
             R.id.btn_positive -> {
                 dismiss()
-                if(btnPositiveClick != null) {
+                if (btnPositiveClick != null) {
+                    util.set(Constants.PASSPHRASE, passphrase_input.text.toString())
                     btnPositiveClick?.onClick(this, DialogInterface.BUTTON_POSITIVE)
                 }
             }
         }
+    }
+
+    private val passphraseTextWatcher: TextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            if(passphrase_input.text.isNotEmpty()) {
+                btn_positive.isEnabled = true
+                btn_positive.setTextColor(ContextCompat.getColor(getContext(), R.color.ButtonColor))
+            } else {
+                btn_positive.isEnabled = false
+                btn_positive.setTextColor(ContextCompat.getColor(getContext(), R.color.greyViewDivider))
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 }
