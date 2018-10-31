98079ef05f27229f54731b800ae1de20d7ca2cec
Collins Olanrewaju <ocollins444@gmail.com>
2018-10-31 11:44:12 +0100

    Fix empty wallet log

--- app/src/main/java/com/dcrandroid/activities/SettingsActivity.java
+++ app/src/main/java/com/dcrandroid/activities/SettingsActivity.java
@@ -212,7 +212,7 @@ public class SettingsActivity extends AppCompatActivity {
             findPreference("dcrwallet_log").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    String logDir = BuildConfig.IS_TESTNET ? "/dcrwallet/logs/testnet3/dcrwallet.log" : "/dcrwallet/logs/mainnet/dcrwallet.log";
+                    String logDir = BuildConfig.IS_TESTNET ? "/wallet/logs/testnet3/dcrwallet.log" : "/wallet/logs/mainnet/dcrwallet.log";
 
                     Intent i = new Intent(getActivity(), LogViewer.class);
                     i.putExtra("log_path", getContext().getFilesDir() + logDir);
