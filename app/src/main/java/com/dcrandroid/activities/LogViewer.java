08185da1fc193bddc2558bea7043e6955133a042
Collins Olanrewaju <ocollins444@gmail.com>
2018-11-06 00:22:52 +0100

    Reverse log output and follow log.

--- app/src/main/java/com/dcrandroid/activities/LogViewer.java
+++ app/src/main/java/com/dcrandroid/activities/LogViewer.java
@@ -3,6 +3,7 @@ package com.dcrandroid.activities;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,6 +18,8 @@ import java.io.File;
  */
 
 public class LogViewer extends AppCompatActivity {
+
+    private TextView logTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,31 +29,58 @@ public class LogViewer extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
         }
         setContentView(R.layout.activity_log_viewer);
-        TextView logTextView = findViewById(R.id.log_text);
-        String logPath = getIntent().getExtras().getString("log_path");
-        if(logPath != null){
-            File f = new File(logPath);
+        logTextView = findViewById(R.id.log_text);
 
-            logTextView.setText(getLastNLogLine(f));
+        logTextView.setText("", TextView.BufferType.EDITABLE);
+
+        buffer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(buffer != null && !buffer.isInterrupted()){
+            buffer.interrupt();
         }
     }
 
-    public String getLastNLogLine(File file) {
-        StringBuilder s = new StringBuilder();
-        try {
-            Process p = Runtime.getRuntime().exec("tail -n500 "+file);
-            java.io.BufferedReader input = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()));
-            String line;
-            int i = 0;
-            while((line = input.readLine()) != null){
-                s.append(line);
-                s.append("\n");
-                i++;
-                System.out.println("++i"+i);
+    Thread buffer = new Thread(){
+        public void run(){
+            try {
+                String logPath = getIntent().getExtras().getString("log_path");
+                if(logPath == null){
+                    return;
+                }
+                File file = new File(logPath);
+                if(!file.exists()){
+                    Snackbar.make(logTextView, R.string.log_file_not_found, Snackbar.LENGTH_LONG).setAction(R.string.dismiss, null).show();
+                    return;
+                }
+
+                Process p = Runtime.getRuntime().exec("tail -f -n500 "+file);
+                java.io.BufferedReader input = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()));
+                String line;
+                while((line = input.readLine()) != null && !interrupted()){
+                    addLine("\n" + line);
+                }
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
             }
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
         }
-        return s.toString();
+    };
+
+    private void addLine(final String line){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(buffer != null && !buffer.isInterrupted()) {
+                    if (logTextView.getEditableText() == null) {
+                        logTextView.setText(line);
+                    } else {
+                        logTextView.getEditableText().insert(0, line);
+                    }
+                }
+            }
+        });
     }
 }
