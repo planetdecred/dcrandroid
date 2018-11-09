babf4aebb1937fb5e0882cf95f60160d2d0b5df9
Collins Olanrewaju <ocollins444@gmail.com>
2018-11-09 14:49:00 +0100

    travis: fix travis build error.
    - Upgrade go version to 1.11
    - Use go modules for dependency management.

--- build.sh
+++
@@ -1,7 +0,0 @@
-go get -v github.com/decred/dcrwallet
-cd $GOPATH/src/github.com/decred/dcrwallet
-git fetch https://github.com/C-ollins/dcrwallet mobile
-git branch mobile FETCH_HEAD
-git checkout mobile
-dep ensure -v
-gomobile bind -target=android/arm
