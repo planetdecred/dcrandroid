879fcafde8969cfa5604a0e2b131955d49fdfb6c
Collins Olanrewaju <ocollins444@gmail.com>
2019-02-14 07:57:10 +0100

    add button in settings to display License

---
+++ app/src/main/java/com/dcrandroid/activities/License.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018-2019 The Decred developers
+ * Use of this source code is governed by an ISC
+ * license that can be found in the LICENSE file.
+ */
+
+package com.dcrandroid.activities
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.dcrandroid.R
+
+class License : AppCompatActivity() {
+    val license = "ISC License\n" +
+            "\n" +
+            "Copyright (c) 2018-2019 The Decred developers\n" +
+            "\n" +
+            "Permission to use, copy, modify, and distribute this software for any\n" +
+            "purpose with or without fee is hereby granted, provided that the above\n" +
+            "copyright notice and this permission notice appear in all copies.\n" +
+            "\n" +
+            "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES\n" +
+            "WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF\n" +
+            "MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR\n" +
+            "ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES\n" +
+            "WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN\n" +
+            "ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF\n" +
+            "OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE."
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val decorView = window.decorView
+            decorView.systemUiVisibility = WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+
+        setContentView(R.layout.activity_license)
+        findViewById<TextView>(R.id.license_text).text = license
+    }
+}
--- app/src/main/java/com/dcrandroid/data/Constants.java
+++ app/src/main/java/com/dcrandroid/data/Constants.java
@@ -116,7 +116,8 @@ public class Constants {
             RECENT_TRANSACTION_HASH = "recent_transaction_hash",
             FIRST_RUN = "first_run",
             TX_NOTIFICATION_HASH = "tx_notification_hash",
-            WIFI_SYNC = "wifi_sync";
+            WIFI_SYNC = "wifi_sync",
+            LICENSE = "license";
     public static final int TRANSACTION_SUMMARY_ID = 5552478, REQUIRED_CONFIRMATIONS = 2;
 
     public static final long ESTIMATED_ACCT_DISCOVERY = 300000;
