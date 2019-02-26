7d4c3d4b7834fda8a2bb1c95b89c15892a8a3561
Collins Olanrewaju <ocollins444@gmail.com>
2019-02-26 06:39:39 +0100

    Fix license text format

--- app/src/main/java/com/dcrandroid/activities/License.kt
+++ app/src/main/java/com/dcrandroid/activities/License.kt
@@ -15,21 +15,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.dcrandroid.R
 
 class License : AppCompatActivity() {
-    val license = "ISC License\n" +
-            "\n" +
-            "Copyright (c) 2018-2019 The Decred developers\n" +
-            "\n" +
-            "Permission to use, copy, modify, and distribute this software for any\n" +
-            "purpose with or without fee is hereby granted, provided that the above\n" +
-            "copyright notice and this permission notice appear in all copies.\n" +
-            "\n" +
-            "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES\n" +
-            "WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF\n" +
-            "MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR\n" +
-            "ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES\n" +
-            "WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN\n" +
-            "ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF\n" +
-            "OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE."
+    val license = "ISC License" +
+            "\n\n" +
+            "Copyright (c) 2018-2019 The Decred developers" +
+            "\n\n" +
+            "Permission to use, copy, modify, and distribute this software for any" +
+            " purpose with or without fee is hereby granted, provided that the above" +
+            " copyright notice and this permission notice appear in all copies." +
+            "\n\n" +
+            "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES" +
+            " WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF" +
+            " MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR" +
+            " ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES" +
+            " WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN" +
+            " ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF" +
+            " OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE."
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
