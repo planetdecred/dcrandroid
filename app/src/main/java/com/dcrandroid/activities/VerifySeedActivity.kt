d5da3a18f44c48800112db11fdc1a6a0cfc1c20f
Collins Olanrewaju <ocollins444@gmail.com>
2019-07-08 23:21:17 +0100

    verify seed: reveal next row when tapping seeds
    This cancels out the need the to scroll manually while using the
    verify seed page.

--- app/src/main/java/com/dcrandroid/activities/VerifySeedActivity.kt
+++ app/src/main/java/com/dcrandroid/activities/VerifySeedActivity.kt
@@ -7,54 +7,39 @@
 package com.dcrandroid.activities
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
-import android.widget.RelativeLayout
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dcrandroid.R
-import com.dcrandroid.adapter.CreateWalletAdapter
-import com.dcrandroid.adapter.InputSeed
-import com.dcrandroid.adapter.MultiSeed
+import com.dcrandroid.adapter.*
 import com.dcrandroid.data.Constants
+import com.dcrandroid.util.Utils
 import dcrlibwallet.Dcrlibwallet
-import kotlinx.android.synthetic.main.confirm_seed_page.*
+import kotlinx.android.synthetic.main.verify_seed_page.*
 
-class VerifySeedActivity : BaseActivity() {
+class VerifySeedActivity : BaseActivity(), SeedTapListener {
 
-    private var verifiedSeed: Boolean = false
-
-    private var finalSeedsString = ""
-
-    private var currentSeedPosition = 0
-
-    private var allSeeds = ArrayList<String>()
-    private val shuffledSeeds = ArrayList<InputSeed>()
-    private val arrayOfSeedLists = ArrayList<MultiSeed>()
-    private var sortedList = listOf<InputSeed>()
-    private val confirmedSeedsArray = ArrayList<InputSeed>()
-    private val arrayOfRandomSeeds = ArrayList<InputSeed>()
+    private var seeds = ArrayList<String>()
+    private lateinit var allSeeds: Array<String>
 
     private lateinit var createWalletAdapter: CreateWalletAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private lateinit var footer: SeedPageFooter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
 
-        setContentView(R.layout.confirm_seed_page)
-        recyclerViewSeeds.isNestedScrollingEnabled = false
+        setContentView(R.layout.verify_seed_page)
+
         linearLayoutManager = LinearLayoutManager(this)
         recyclerViewSeeds.layoutManager = linearLayoutManager
-        button_confirm_seed.setOnClickListener {
-            if (verifiedSeed) {
-                val intent = Intent(this, EncryptWallet::class.java)
-                intent.putExtra(Constants.SEED, finalSeedsString)
-                startActivity(intent)
-            }
-        }
+
+        footer = SeedPageFooter(null, R.drawable.btn_shape2)
+
+        allSeeds = Utils.getWordList(this).split(" ").toTypedArray()
+
         prepareData()
     }
 
@@ -62,121 +47,103 @@ class VerifySeedActivity : BaseActivity() {
         val bundle = intent.extras
         if (bundle != null && !bundle.isEmpty) {
             val seed = bundle.getString(Constants.SEED)
-            if(seed != null) {
-                allSeeds = ArrayList(seed.split(" "))
-                tvHeader.text = getString(R.string.seed_phrase_verification)
-                tvHint.text = getString(R.string.please_confirm_your_seed_by_typing_and_tapping_each_word_accordingly)
-                headerTop.isFocusableInTouchMode = true
+            if (seed != null) {
+                seeds = ArrayList(seed.split(" "))
                 initSeedAdapter()
+                getMultiSeedList()
             }
         }
+    }
+
+    private fun getMultiSeedList(): ArrayList<MultiSeed> {
+        val multiSeedList = ArrayList<MultiSeed>()
+        for (seed in seeds) {
+            multiSeedList.add(getMultiSeed(allSeeds.indexOf(seed)))
+        }
+
+        return multiSeedList
+    }
+
+    private fun getMultiSeed(realSeedIndex: Int): MultiSeed {
+
+        val list = (0 until 33).toMutableList()
+        list.remove(realSeedIndex)
+
+        val firstRandom = list.random()
+        val firstInputSeed = InputSeed(firstRandom, allSeeds[firstRandom])
+        list.remove(firstRandom)
+
+        val secondRandom = list.random()
+        val secondInputSeed = InputSeed(secondRandom, allSeeds[secondRandom])
+
+        val realInputSeed = InputSeed(realSeedIndex, allSeeds[realSeedIndex])
+
+        val arr = ArrayList<InputSeed>()
+        arr.add(firstInputSeed)
+        arr.add(secondInputSeed)
+        arr.add(realInputSeed)
+
+        arr.shuffle()
+
+        return MultiSeed(arr[0], arr[1], arr[2])
     }
 
     private fun initSeedAdapter() {
-        createWalletAdapter = CreateWalletAdapter(applicationContext, arrayOfSeedLists, { enteredSeeds: ArrayList<InputSeed> ->
-            confirmedSeedsArray.clear()
-            confirmedSeedsArray.addAll(enteredSeeds)
-            sortedList = confirmedSeedsArray.sortedWith(compareBy { it.number }).distinct()
-
-            if (sortedList.size < 33) {
-                tvError.visibility = View.VISIBLE
-                tvError.text = getString(R.string.notAllSeedsEntered)
-            }
-            val itemView = recyclerViewSeeds.findViewById<RelativeLayout>(R.id.rlButtons)
-            val itemHeight = itemView.measuredHeight
-            val maxAllowedHeight = nestedScrollView.getChildAt(0).bottom - (itemView.measuredHeight + itemView.measuredHeight / 2)
-
-            val currentHeight = (nestedScrollView.scrollY + nestedScrollView.height)
-            if (currentHeight > maxAllowedHeight) {
-                nestedScrollView.smoothScrollBy(0, itemHeight * 2)
-            }
-        }, { isAllEntered: Boolean ->
-            if (isAllEntered && sortedList.size == 33) {
-                handleSingleTap()
-            }
-        })
+        val allSeedWords = getMultiSeedList()
+        createWalletAdapter = CreateWalletAdapter(this, allSeedWords, this, footer)
         recyclerViewSeeds.adapter = createWalletAdapter
-        generateRandomSeeds()
     }
 
-    private fun generateRandomSeeds() {
-        val firstRandom = (0 until allSeeds.size).random()
-        val secondRandom = (0 until allSeeds.size).random()
-        var currentItemPosition = 0
-        if (currentSeedPosition != 33) {
-            currentItemPosition = (currentSeedPosition)
-        }
-
-        if (firstRandom != secondRandom && firstRandom != currentItemPosition && secondRandom != currentItemPosition) {
-            for (item in allSeeds) {
-                when (item) {
-                    allSeeds[firstRandom] -> arrayOfRandomSeeds.add(InputSeed(firstRandom, item))
-                    allSeeds[secondRandom] -> arrayOfRandomSeeds.add(InputSeed(secondRandom, item))
-                    allSeeds[currentItemPosition] -> arrayOfRandomSeeds.add(InputSeed(currentItemPosition, item))
-                }
-            }
-            addSeedsToAdapter()
-        } else {
-            generateRandomSeeds()
-        }
+    private fun notAllSeedsEntered() {
+        footer.error = getString(R.string.notAllSeedsEntered)
+        // 34: last item on the last after adding up 1 header and 33 seed rows(using 0th index)
+        createWalletAdapter.notifyItemChanged(34)
     }
 
-    private fun addSeedsToAdapter() {
-        shuffledSeeds.addAll(arrayOfRandomSeeds.shuffled().distinct())
-        if (shuffledSeeds.size == 3) {
-            when {
-                currentSeedPosition < 32 -> {
-                    arrayOfSeedLists.add(MultiSeed(shuffledSeeds[0], shuffledSeeds[1], shuffledSeeds[2]))
-                    createWalletAdapter.notifyDataSetChanged()
-                    currentSeedPosition++
-                    arrayOfRandomSeeds.clear()
-                    shuffledSeeds.clear()
-                    generateRandomSeeds()
-                }
-                currentSeedPosition == 32 -> {
-                    arrayOfSeedLists.add(MultiSeed(shuffledSeeds[0], shuffledSeeds[1], shuffledSeeds[2]))
-                    createWalletAdapter.notifyDataSetChanged()
-                    arrayOfRandomSeeds.clear()
-                    shuffledSeeds.clear()
-                }
-            }
-        } else {
-            arrayOfRandomSeeds.clear()
-            shuffledSeeds.clear()
-            generateRandomSeeds()
+    override fun onConfirm(enteredSeeds: ArrayList<String>, emptySeed: Boolean) {
+
+        if (emptySeed) {
+            notAllSeedsEntered()
+            return
         }
+
+        val seedString = enteredSeeds.joinToString(" ", "", "", -1, "...")
+
+        if (Dcrlibwallet.verifySeed(seedString)) {
+            footer.buttonBackground = R.drawable.btn_shape3
+            footer.error = null
+
+            val intent = Intent(this, EncryptWallet::class.java)
+            intent.putExtra(Constants.SEED, seedString)
+            startActivity(intent)
+        } else {
+            footer.buttonBackground = R.drawable.btn_shape2
+            footer.error = getString(R.string.create_wallet_incorrect_seeds_input)
+        }
+
+
+        createWalletAdapter.notifyItemChanged(34)
     }
 
-    private fun handleSingleTap() {
+    override fun onSeedEntered(enteredSeeds: ArrayList<String>, emptySeed: Boolean, position: Int) {
 
-        if (sortedList.isNotEmpty() && (sortedList.size == 33)) {
-            finalSeedsString = sortedList.joinToString(" ", "", "", -1, "...") { it.phrase }
-            verifiedSeed = Dcrlibwallet.verifySeed(finalSeedsString)
+        linearLayoutManager.scrollToPosition(position + 2)
 
-            if (verifiedSeed) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    button_confirm_seed.background = ContextCompat.getDrawable(applicationContext, R.drawable.btn_shape3)
-                } else {
-                    button_confirm_seed.setBackgroundDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.btn_shape3))
-                }
-
-                tvError.visibility = View.GONE
-
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    button_confirm_seed.background = ContextCompat.getDrawable(applicationContext, R.drawable.btn_shape2)
-                } else {
-                    button_confirm_seed.setBackgroundDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.btn_shape2))
-                }
-
-                tvError.visibility = View.VISIBLE
-                tvError.text = getString(R.string.create_wallet_incorrect_seeds_input)
-            }
-
-        } else if (sortedList.isNotEmpty() && (sortedList.size != 33)) {
-            tvError.text = getString(R.string.notAllSeedsEntered)
-        } else {
-            tvError.text = getString(R.string.theInputFieldIsEmpty)
+        if (emptySeed) {
+            notAllSeedsEntered()
+            return
         }
+
+        val seedString = enteredSeeds.joinToString(" ", "", "", -1, "...")
+        if (Dcrlibwallet.verifySeed(seedString)) {
+            footer.buttonBackground = R.drawable.btn_shape3
+            footer.error = null
+        } else {
+            footer.buttonBackground = R.drawable.btn_shape2
+            footer.error = getString(R.string.create_wallet_incorrect_seeds_input)
+        }
+
+        // 34: last item on the last after adding up 1 header and 33 seed rows(using 0th index)
+        createWalletAdapter.notifyItemChanged(34)
     }
 }
