bedac50fd1a7373ff4d5ff2941621808f44533e2
cheruiyot <skipcheru@gmail.com>
2019-08-01 16:24:28 +0300

    Feature: show spending password and pin dialog on click create wallet

--- app/src/main/java/com/dcrandroid/activities/SetupWalletActivity.java
+++ app/src/main/java/com/dcrandroid/activities/SetupWalletActivity.java
@@ -12,35 +12,42 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
 
+import androidx.core.app.ActivityCompat;
+
+import com.dcrandroid.MainActivity;
 import com.dcrandroid.R;
 import com.dcrandroid.data.Constants;
+import com.dcrandroid.ui.security.PasswordPinDialogFragment;
 import com.dcrandroid.util.PreferenceUtil;
-import com.dcrandroid.util.Utils;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Macsleven on 25/12/2017.
  */
 
-public class SetupWalletActivity extends BaseActivity {
+public class SetupWalletActivity extends BaseActivity implements PasswordPinDialogFragment.PasswordPinListener {
+
+    private PreferenceUtil preferenceUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_setup_page);
+
+        preferenceUtil = new PreferenceUtil(this);
+
         LinearLayout restoreView = findViewById(R.id.ll_restore_wallet);
         LinearLayout createView = findViewById(R.id.ll_create_wallet);
 
-        final PreferenceUtil preferenceUtil = new PreferenceUtil(this);
         final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.7F);
 
         createView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 view.startAnimation(buttonClick);
-                preferenceUtil.setBoolean(Constants.RESTORE_WALLET, false);
-                Intent i = new Intent(SetupWalletActivity.this, SaveSeedActivity.class);
-                startActivity(i);
+                showPassWordPinDialog();
             }
         });
 
@@ -48,12 +55,51 @@ public class SetupWalletActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 view.startAnimation(buttonClick);
-                preferenceUtil.setBoolean(Constants.RESTORE_WALLET, true);
-                Intent i = new Intent(SetupWalletActivity.this, ConfirmSeedActivity.class)
-                        .putExtra(Constants.SEED, Utils.getWordList(SetupWalletActivity.this))
-                        .putExtra(Constants.RESTORE, true);
-                startActivity(i);
+                navigateToRestoreSeedWorkflow();
             }
         });
+    }
+
+    /**
+     * Callback when the user submits spending password or pin
+     *
+     * @param spendingKey - either a spending password or pin
+     * @param isPassword  - flag to tell whether its a password or pin
+     */
+    @Override
+    public void onEnterPasswordOrPin(@NotNull String spendingKey, boolean isPassword) {
+        if (isPassword) {
+            preferenceUtil.set(Constants.SPENDING_PASSPHRASE_TYPE, Constants.PASSWORD);
+        } else {
+            preferenceUtil.set(Constants.SPENDING_PASSPHRASE_TYPE, Constants.PIN);
+        }
+        navigateToMainActivity(spendingKey);
+    }
+
+    /**
+     * Shows the spending pin and password bottom sheet dialog
+     */
+    private void showPassWordPinDialog() {
+        PasswordPinDialogFragment passwordPinDialog = new PasswordPinDialogFragment();
+        passwordPinDialog.show(getSupportFragmentManager(), "passwordPinDialog");
+    }
+
+    /**
+     * Navigates user to main activity
+     *
+     * @param spendingKey - spending password or pin
+     */
+    private void navigateToMainActivity(String spendingKey) {
+        preferenceUtil.setBoolean(Constants.RESTORE_WALLET, false);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(Constants.PASSPHRASE, spendingKey);
+        startActivity(intent);
+        ActivityCompat.finishAffinity(this);
+    }
+
+    private void navigateToRestoreSeedWorkflow() {
+        preferenceUtil.setBoolean(Constants.RESTORE_WALLET, true);
+        // TODO - hook up restore wallet workflow after implementing restore wallet UI
     }
 }
