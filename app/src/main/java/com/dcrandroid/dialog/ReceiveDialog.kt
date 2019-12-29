84921a76e06576d87500b8825f4fd49781aee007
Olanrewaju Collins <ocollins444@gmail.com>
2019-12-29 15:49:22 +0100

    receive: place Decred logo in QR code center (#420)
    * receive: place Decred logo in QR code center
    
    * receive: add curved corners to qr code logo and restore white background
    
    * receive: draw DCR logo on QR code bitmap
    
    The previous implementation placed the logo on the QR code using android
    layout. When sharing the QR code to other apps, the logo doesn't go with it,
    which is why it is now drawn on it.

--- app/src/main/java/com/dcrandroid/dialog/ReceiveDialog.kt
+++ app/src/main/java/com/dcrandroid/dialog/ReceiveDialog.kt
@@ -8,14 +8,19 @@ package com.dcrandroid.dialog
 
 import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.*
+import android.graphics.drawable.VectorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.dcrandroid.BuildConfig
 import com.dcrandroid.R
 import com.dcrandroid.adapter.PopupItem
@@ -91,13 +96,47 @@ class ReceiveDialog(dismissListener: DialogInterface.OnDismissListener) : FullSc
         Utils.copyToClipboard(top_bar, tv_address.text.toString(), R.string.address_copy_text)
     }
 
+    private fun getLogoBitmap(): Bitmap{
+        val logoDrawable = AppCompatResources.getDrawable(context!!, R.drawable.ic_qr_dcr)!!
+        val sizePixels = resources.getDimensionPixelOffset(R.dimen.margin_padding_size_80)
+        val bitmap = Bitmap.createBitmap(sizePixels, sizePixels, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        logoDrawable.setBounds(0, 0, canvas.width, canvas.height)
+
+        val rect = Rect(0, 0, canvas.width, canvas.height)
+        val rectF = RectF(rect)
+        val paint = Paint()
+        paint.style = Paint.Style.FILL
+        paint.color = Color.WHITE
+        paint.isAntiAlias = true
+
+        val radius = resources.getDimension(R.dimen.margin_padding_size_4)
+        canvas.drawRoundRect(rectF, radius, radius, paint)
+        logoDrawable.draw(canvas)
+
+
+        return bitmap
+    }
+
+    private fun overlayLogo(qrBitmap: Bitmap): Bitmap {
+        val overlay = Bitmap.createBitmap(qrBitmap.width, qrBitmap.height, qrBitmap.config)
+
+        val logo = getLogoBitmap()
+        val canvas = Canvas(overlay)
+        canvas.drawBitmap(qrBitmap, 0f, 0f, null)
+        canvas.drawBitmap(logo, ((qrBitmap.width - logo.width) / 2).toFloat(), ((qrBitmap.height - logo.height) / 2).toFloat(), null)
+        return overlay
+    }
+
     private fun setAddress(address: String) = GlobalScope.launch(Dispatchers.Main) {
         tv_address.text = address
 
         // Generate QR Code
         val barcodeEncoder = BarcodeEncoder()
-        val generatedQR = barcodeEncoder.encodeBitmap(getString(R.string.decred_qr_address_prefix, address),
+        var generatedQR = barcodeEncoder.encodeBitmap(getString(R.string.decred_qr_address_prefix, address),
                 BarcodeFormat.QR_CODE, 1000, 1000, qrHints)
+
+        generatedQR = overlayLogo(generatedQR)
 
         qr_image.setImageBitmap(generatedQR)
 
