6f2957538d6fd5aeb8eb6cba370e03e0c42f6850
Collins Olanrewaju <ocollins444@gmail.com>
2020-06-02 07:17:31 +0100

    use macos for builds

--- dcrlibwallet-ci-build.sh
+++ dcrlibwallet-ci-build.sh
@@ -1,8 +1,8 @@
 installGo(){
     echo "Installing golang"
-    curl -O https://storage.googleapis.com/golang/go1.12.9.linux-amd64.tar.gz
-    sha256sum go1.12.9.linux-amd64.tar.gz
-    tar -xvf go1.12.9.linux-amd64.tar.gz
+    curl -O https://storage.googleapis.com/golang/go1.12.9.darwin-amd64.tar.gz
+    sha256sum go1.12.9.darwin-amd64.tar.gz
+    tar -xvf go1.12.9.darwin-amd64.tar.gz
     sudo chown -R root:root ./go
     sudo mv go /usr/local
 }
