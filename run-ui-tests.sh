55517d300fab02fa3375fe4e2fbdcb689f492b91
Collins Olanrewaju <ocollins444@gmail.com>
2020-06-02 08:40:49 +0100

    Wait for device before starting test

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -2,6 +2,7 @@ echo no | $ANDROID_HOME/tools/bin/avdmanager create avd --force -n testemu --abi
 $ANDROID_HOME/tools/bin/avdmanager list avd
 $ANDROID_HOME/tools/emulator -avd testemu -no-window -no-boot-anim -no-audio -verbose &
 
+$ANDROID_HOME/platform-tools/adb wait-for-device
 
 # emulator isn't ready yet, wait 1 min more
 # prevents APK installation error
