9458a3c99eff2d3b08e463a697c28646a1c402c8
Olanrewaju Collins <ocollins444@gmail.com>
2020-06-30 04:39:47 +0100

    Add Espresso tests and replace travis test with GitHub Actions (#481)
    * Add tests
    
    * Replace travis ci with github workflow
    
    * Make dcrlibwallet executable
    
    * Rename dcrlibwallet.sh to dcrlibwallet-ci-build.sh
    
    * Upload lint results after running android linter
    
    * Upload testnet apk after building
    
    * Run tests on emulator after build
    
    * use macos for builds
    
    * Run emulator manually
    
    * Wait for device before starting test
    
    * Install android emulator before attempting to create
    
    * Cache gradle dependencies and accept android sdk licenses
    
    * Silence ndk download log
    
    Change gradle cache keys as an attempt to fix cache error
    
    * Use android emulator runner
    
    * Disable emulator and retry gradle cache
    
    * Add ndk to cache
    
    * Add go mod to cache
    
    * Remove ndk installation step
    
    * Add wallet restore test
    
    * Add seed backup test

---
+++ app/src/androidTest/java/com/dcrandroid/Matchers.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2018-2019 The Decred developers
+ * Use of this source code is governed by an ISC
+ * license that can be found in the LICENSE file.
+ */
+
+package com.dcrandroid
+
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+internal fun childAtPosition(parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Child at position $position in parent ")
+            parentMatcher.describeTo(description)
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            val parent = view.parent
+            return parent is ViewGroup && parentMatcher.matches(parent)
+                    && view == parent.getChildAt(position)
+        }
+    }
+}
+
+internal fun childHasParentId(parentId: Int): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Has parent Id ")
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            var viewParent: View? = view.parent as View
+            do {
+                if (viewParent!!.id == parentId) {
+                    return true
+                }
+
+                viewParent = viewParent.parent as? View // this casting will return null if it's not a View instance
+            } while (viewParent != null)
+
+            return false
+        }
+    }
+}
+
+internal fun childHasParentMatcher(parentMatcher: Matcher<View>): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Child has parent matcher ")
+            parentMatcher.describeTo(description)
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            var viewParent: View? = view.parent as? View
+            do {
+                if (parentMatcher.matches(viewParent!!)) {
+                    return true
+                }
+
+                viewParent = viewParent.parent as? View // this casting will return null if it's not a View instance
+            } while (viewParent != null)
+
+            return false
+        }
+    }
+}
+
+internal fun isImmediateChild(parentMatcher: Matcher<View>): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("isImmediateChild")
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            val parent = view.parent
+            return parent is ViewGroup && parentMatcher.matches(parent)
+        }
+    }
+}
