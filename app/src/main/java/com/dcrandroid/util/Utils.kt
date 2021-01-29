9869fa7e978ddabb50edd1dbf627f28df6aaa2ee
dreacot <kennedyizuegbu@yahoo.com>
2021-01-29 15:32:50 +0100

    add different notification messages for vote transactions

--- app/src/main/java/com/dcrandroid/util/Utils.kt
+++ app/src/main/java/com/dcrandroid/util/Utils.kt
@@ -165,10 +165,22 @@ object Utils {
         val multiWallet = WalletData.multiWallet!!
         val wallet = multiWallet.walletWithID(transaction.walletID)
 
-        val title = if (multiWallet.openedWalletsCount() > 1) {
-            context.getString(R.string.wallet_new_transaction, wallet.name)
-        } else {
-            context.getString(R.string.new_transaction)
+        val title = when (transaction.type) {
+            Dcrlibwallet.TxTypeVote -> context.getString(R.string.ticket_voted)
+            Dcrlibwallet.TxTypeRevocation -> context.getString(R.string.ticket_revoked)
+            else ->
+                if (multiWallet.openedWalletsCount() > 1) {
+                    context.getString(R.string.wallet_new_transaction, wallet.name)
+                } else {
+                    context.getString(R.string.new_transaction)
+                }
+        }
+
+        val amount = when (transaction.type) {
+            Dcrlibwallet.TxTypeVote -> context.getString(R.string.vote_reward, transaction.voteReward)
+            Dcrlibwallet.TxTypeRevocation -> ""
+            else ->
+                amount
         }
 
         val incomingNotificationsKey = transaction.walletID.toString() + Dcrlibwallet.IncomingTxNotificationsConfigKey
