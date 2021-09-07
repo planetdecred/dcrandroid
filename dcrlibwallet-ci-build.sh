db0cc7768deb9bfc8b04c77f9176972e50565d52
Olanrewaju Collins <ocollins444@gmail.com>
2021-09-07 17:55:57 +0100

    Add settings option to choose app theme (#582)
    * Add settings option to choose app theme
    
    * ci: use go 1.16.7
    
    * Fix ci error

--- dcrlibwallet-ci-build.sh
+++ dcrlibwallet-ci-build.sh
@@ -1,14 +1,15 @@
 installGo(){
     echo "Installing golang"
-    curl -O https://storage.googleapis.com/golang/go1.12.9.darwin-amd64.tar.gz
-    sha256sum go1.12.9.darwin-amd64.tar.gz
-    tar -xvf go1.12.9.darwin-amd64.tar.gz
+    curl -O https://storage.googleapis.com/golang/go1.16.7.darwin-amd64.tar.gz
+    sha256sum go1.16.7.darwin-amd64.tar.gz
+    tar -xvf go1.16.7.darwin-amd64.tar.gz
     sudo chown -R root:root ./go
     sudo mv go /usr/local
 }
 
 installGomobile(){
     echo "Installing gomobile"
+    export GO111MODULE=off
     go get -u golang.org/x/mobile/cmd/gomobile
     gomobile init
 }
