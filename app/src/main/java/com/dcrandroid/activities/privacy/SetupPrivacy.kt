604ae278d24b012360f84aa12126713f2922f550
Jibirin-Yaro <yazeed.jibirin-yaro@Terra-103.local>
2022-04-08 13:48:54 +0100

    Check for more than one account

--- app/src/main/java/com/dcrandroid/activities/privacy/SetupPrivacy.kt
+++ app/src/main/java/com/dcrandroid/activities/privacy/SetupPrivacy.kt
@@ -8,10 +8,15 @@ package com.dcrandroid.activities.privacy
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.widget.Toast
 import com.dcrandroid.R
 import com.dcrandroid.activities.BaseActivity
 import com.dcrandroid.data.Constants
+import com.dcrandroid.dialog.InfoDialog
 import dcrlibwallet.Wallet
+import kotlinx.android.synthetic.main.account_row.*
 import kotlinx.android.synthetic.main.activity_setup_privacy.*
 
 class SetupPrivacy : BaseActivity() {
@@ -25,10 +30,19 @@ class SetupPrivacy : BaseActivity() {
         wallet_name.text = wallet.name
 
         btn_setup_mixer.setOnClickListener {
-            val intent = Intent(this, SetupMixerAccounts::class.java)
-            intent.putExtra(Constants.WALLET_ID, wallet.id)
-            finish()
-            startActivity(intent)
+            if (wallet.accountsRaw.count > 1) {
+                val intent = Intent(this, SetupMixerAccounts::class.java)
+                intent.putExtra(Constants.WALLET_ID, wallet.id)
+                finish()
+                startActivity(intent)
+            }
+            else {
+                InfoDialog(this)
+                    .setDialogTitle(getString(R.string.mixer_setup_error_title))
+                    .setMessage(getString(R.string.mixer_setup_error_desc))
+                    .setNegativeButton(getString(R.string.ok))
+                    .show()
+            }
         }
 
         go_back.setOnClickListener { finish() }
